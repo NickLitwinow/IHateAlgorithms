2bc971d2034c4cffa09c7a74f8fea89601c8c703
litwein <nicklitvinow@gmail.com>
2025-01-24 21:47:59 +0700

    - Дана строка - 1 done

--- DE-2025 Div2A - строки/Дана строка - 1/main.cpp
+++ DE-2025 Div2A - строки/Дана строка - 1/main.cpp
@@ -15,17 +15,17 @@ int main () {
         ba_counter++;
       }
     }
-    if (ab_counter > ba_counter || ba_counter > ab_counter) {
-      for (int j = 1; j < t.length(); j++) {
-        if (t[j] == 'b' && t[j - 1] == 'a') {
-          ab_counter++;
-        }
-        if (t[j] == 'a' && t[j - 1] == 'b') {
-          ba_counter++;
-        }
+    if (ab_counter == ba_counter) {
+      cout << t << endl;
+    } else {
+      if (ab_counter > ba_counter) {
+        t[0] = 'b';
+      } else {
+        t[0] = 'a';
       }
+      cout << t << endl;
     }
-    cout << t << endl;
+
     ab_counter = 0;
     ba_counter = 0;
   }
