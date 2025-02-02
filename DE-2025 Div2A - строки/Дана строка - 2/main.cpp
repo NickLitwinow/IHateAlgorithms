88480781330dee67dafa22f78a4c61acdebb3f2f
litwein <nicklitvinow@gmail.com>
2025-02-02 21:50:56 +0700

    - Дана строка - 2 done

--- DE-2025 Div2A - строки/Дана строка - 2/main.cpp
+++ DE-2025 Div2A - строки/Дана строка - 2/main.cpp
@@ -1,3 +1,36 @@
-//
-// Created by Litwein on 23.01.2025.
-//
+#include <iostream>
+using namespace std;
+
+bool isPalindrom(string s){
+  for(int i = 0; i < s.size()/2; i++){
+    if(s[i] != s[s.size()-i-1]) return false;
+  }
+  return true;
+}
+
+int main () {
+  int t;
+  cin >> t;
+  while(t--){
+    string s;
+    cin >> s;
+    if(!isPalindrom(s)){
+      cout << s << endl;
+      continue;
+    }
+    bool check = true;
+    int n = s.size();
+    for(int i = 0; i < n - 1; i++){
+      if(s[i] != s[i+1]){
+        check = false;
+        swap(s[i], s[i+1]);
+        cout << s << endl;
+        break;
+      }
+    }
+    if(check){
+      cout << -1 << endl;
+    }
+  }
+  return 0;
+}
