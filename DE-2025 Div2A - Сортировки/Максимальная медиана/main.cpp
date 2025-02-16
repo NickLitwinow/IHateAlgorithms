29dacbd2c5c770485b2d06f526a05fec194741eb
litwein <nicklitvinow@gmail.com>
2025-02-16 23:06:45 +0700

    - Максимальная медиана

--- DE-2025 Div2A - Сортировки/Максимальная медиана/main.cpp
+++ DE-2025 Div2A - Сортировки/Максимальная медиана/main.cpp
@@ -4,9 +4,10 @@
 using namespace std;
 
 int main() {
-  int n, k;
+  int n;
+  long long k;
   cin >> n >> k;
-  vector<int> a(n);
+  vector<long long> a(n);
 
   for (int i = 0; i < n; ++i) {
     cin >> a[i];
@@ -14,14 +15,25 @@ int main() {
 
   sort(a.begin(), a.end());
   int median_index = n / 2;
-  for (int i = median_index; i < n - 1 && k > 0; i++) {
-    int next_value = a[i + 1];
-    int diff = next_value - a[i];
-    int steps = min(diff * (i - median_index + 1), k);
-    a[i] += steps / (i - median_index + 1);
-    k -= steps;
+  long long current = a[median_index];
+  int count = 1;
+
+  for (int i = median_index + 1; i < n && k > 0; i++) {
+    long long diff = a[i] - current;
+    if (diff * count <= k) {
+      k -= diff * count;
+      current = a[i];
+      count++;
+    } else {
+      current += k / count;
+      k = 0;
+      break;
+    }
   }
-  a[median_index] += k / (n - median_index);
-  cout << a[median_index] << endl;
+  if (k > 0) {
+    current += k / count;
+  }
+
+  cout << current << endl;
   return 0;
 }
